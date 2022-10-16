852b5bebc818bd001126869fa1f4234db7f56551
hiiksyusha <korovina.kv@phystech.edu>
2022-10-16 22:10:06 +0300

    add sol3

--- 01-lsof/solution.c
+++ 01-lsof/solution.c
@@ -13,6 +13,8 @@
 #include <errno.h>
 #include <string.h>
 
+#define N = 2000
+
 /**
    Implement this function to list processes. It must call report_file()
    for each open file. If an error occurs when accessing a file or
@@ -40,7 +42,7 @@ void lsof(void){
         
 		if (pid == 0) continue; 
         
-	char fdpath[1000] = {0}, flpath[2000] = {0}, allpath[1000] = {0};
+	char fdpath[N] = {0}, flpath[2*N] = {0}, allpath[N] = {0};
         
 	sprintf(fdpath, "/proc/%d/fd", pid);
         
@@ -60,12 +62,12 @@ void lsof(void){
     	int fd = atoi(fd_file->d_name);
 		if (fd == 0) continue;
             
-        memset(flpath, 0, 1000);
-        memset(allpath, 0, 1000);
+        memset(flpath, 0, N);
+        memset(allpath, 0, N);
             
         sprintf(flpath, "%s/%d", fdpath, fd);
             
-       	int count = readlink(flpath, allpath, 1000); //считывает значение символьной ссылки  
+       	int count = readlink(flpath, allpath, N); //считывает значение символьной ссылки
             
         if (count < 0) {
         	report_error(flpath, errno);
