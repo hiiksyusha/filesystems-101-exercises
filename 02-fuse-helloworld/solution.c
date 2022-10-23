243974d8132fcff0ed347a0d1f27802a68e8fa9c
hiiksyusha <korovina.kv@phystech.edu>
2022-10-23 21:40:36 +0300

    fix open5

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -66,7 +66,7 @@ static int my_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t
 
 static int my_open(const char *path, struct fuse_file_info *fi) {
     
-    void(fi);
+    (void)fi;
 
     if ((fi->flags & O_ACCMODE) != O_RDONLY)
         return -EROFS;
